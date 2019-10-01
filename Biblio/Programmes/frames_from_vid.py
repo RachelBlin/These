810397a09b0c14be051ace27fa2f5c3c44d3b228
Rachel <rachel.blin@insa-rouen.fr>
2019-10-01 17:38:57 +0200

    Supression of folders to move them to another project

--- Biblio/Programmes/frames_from_vid.py
+++
@@ -1,9 +0,0 @@
-import cv2
-vidcap = cv2.VideoCapture('/home/rblin/Documents/Databases/11_05_2019/videos/GP040399.MP4')
-success,image = vidcap.read()
-count = 0
-while success:
-  cv2.imwrite("/home/rblin/Documents/Databases/11_05_2019/frames/vid5/frame%d.png" % count, image)     # save frame as png file
-  success,image = vidcap.read()
-  print('Read a new frame: ', success)
-  count += 1
